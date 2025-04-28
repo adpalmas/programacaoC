688335abe587af623265b9b6a00f06422f578000
adpalmas <adpalmas@hotmail.com>
2025-04-28 11:57:02 +0000

    Adicionar arquivo de configuração de tarefas para compilação em C

--- aula01.c
+++ aula01.c
@@ -2,7 +2,24 @@
 
 int main(){
 
+int idade, matricula;
+float altura;
+char nome[50];
 
+printf("Digite sua idade: \n");
+scanf("%d", &idade);
 
+printf("Digite sua altura \n");
+scanf("%s", &altura);
+
+printf("Digite seu nome \n");
+scanf("%s", &nome);
+
+printf("Digite sua matricula \n");
+scanf("%d", &matricula);
+
+printf("Nome do aluno: %s \nMatricula: %d \n", nome, matricula);
+printf("Idade %d \n - altura %f \n", idade, altura);
+return 0;
 
 }
