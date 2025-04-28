ca28f806ea60f22810336cc760c63933135dfabf
adpalmas <adpalmas@hotmail.com>
2025-04-28 20:25:11 +0000

    Adicionar implementação do programa de incremento em C

---
+++ exerc05.c
@@ -0,0 +1,18 @@
+int main() {
+    /*
+    Incremento (++)
+    Pré-Incremento ++a
+    Pós-Incremento a++
+    Decremento (--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
+
+    int numero1 = 1;
+    
+    printf("Antes incremento: %d\n", numero1);
+    numero1++;
+    printf("Pós incremento: %d\n", numero1++);
+    
+    
+}
