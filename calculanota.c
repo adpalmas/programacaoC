63616e8dd603f4c9a338fcf7ef841dd6236c9966
adpalmas <adpalmas@hotmail.com>
2025-04-29 01:50:08 +0000

    Adicionar suporte para quatro notas e calcular a média corretamente

--- calculanota.c
+++ calculanota.c
@@ -1,8 +1,8 @@
 #include <stdio.h> // Inclui a biblioteca padrão de entrada e saída
 
 int main() {
-    float nota1, nota2, nota3;
-    float media;
+    float nota1, nota2, nota3, nota4;
+    float soma;
 
     printf("***Programa de calculo de média ***\n");
     printf("Digite a primeira nota: ");
@@ -11,12 +11,14 @@ int main() {
     scanf("%f", &nota2);
     printf("Digite a terceira nota: ");
     scanf("%f", &nota3);
+    printf("Digite a quarta nota: ");
+    scanf("%f", &nota4);
 
     // Calcula a média
-    media = (nota1 + nota2 + nota3) / 3;
+    soma = (float)(nota1 + nota2 + nota3 + nota4);
 
     // Exibe a média
-    printf("A média é: %f\n", media);
+    printf("A média é: %.2f\n", soma);
 
     return 0; // Indica que o programa terminou com sucesso
 }
